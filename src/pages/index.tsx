b51265a77f4882142e21cb1029ad840d73f2c0b0
Samuel Foletto <sfoletto@gmail.com>
2026-01-05 21:53:11 -0300

    Criação do Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,42 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import heroImg from '../../public/assets/hero.png'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title> Tarefas + </title>
-
       </Head>
-      <h1>Meu projeto</h1>
-    </>
+        <main className={styles.main}>
+            <div className={styles.logoContent}>
+                <Image
+                    className={styles.logo}
+                    alt="logo-tarefas"
+                    src={heroImg}
+                    priority={true}
+                />
+            </div>
+            <h1 className={styles.title}>
+                Sistema feito para você organizar
+                <br/>
+                seus estudos e tarefas
+            </h1>
+
+            <div className={styles.infoContent}>
+                <section className={styles.box}>
+                    <span>+ 12 posts</span>
+                </section>
+                <section className={styles.box}>
+                    <span>+ 90 comentários</span>
+                </section>
+            </div>
+
+
+        </main>
+
+    </div>
   );
 }
